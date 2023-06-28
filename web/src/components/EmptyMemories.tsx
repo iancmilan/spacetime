8184f57f6099f6a849d6d7842da1290e84765d49
ian <iancmilan@unochapeco.edu.br>
2023-06-28 20:55:11 -0300

    adicionando componentes 🛠

---
+++ web/src/components/EmptyMemories.tsx
@@ -0,0 +1,13 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="leading-relax w-[360px] text-center">
+        Você ainda não criou nenhuma lembrança, comece a{' '}
+        <a href="" className="underline hover:text-gray-50">
+          criar agora
+        </a>
+        !
+      </p>
+    </div>
+  )
+}
